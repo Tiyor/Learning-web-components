861a4188ba4950b1c04a07491c383cc4e022bc35
Ikhtiyorjon Abdupattoev <iabdupattoev@gmail.com>
2018-01-22 09:37:53 -0800

    Update main.js

--- Nav/main.js
+++ Nav/main.js
@@ -34,7 +34,7 @@
 
               $('html, body').stop().animate({
                   'scrollTop': $target.offset().top
-              }, 4000, 'swing', function() {
+              }, 2000, 'swing', function() {
                   window.location.hash = target;
               });
           });
