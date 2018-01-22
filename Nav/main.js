d26cee8c85f0a8c845edde6e1d6d40974f316058
Ikhtiyorjon Abdupattoev <iabdupattoev@gmail.com>
2018-01-21 18:07:00 -0800

    Update main.js

--- Nav/main.js
+++ Nav/main.js
@@ -1,42 +1,41 @@
+      // Progress bar functionality
+      // When the user scrolls the page, execute myFunction 
+      window.onscroll = function() {
+          myFunction(), scrollFunction();
+      };
 
-// Progress bar functionality
-// When the user scrolls the page, execute myFunction 
-window.onscroll = function() {
-    myFunction(), scrollFunction();
-};
+      function myFunction() {
+          var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+          var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+          var scrolled = (winScroll / height) * 100;
+          document.getElementById("myBar").style.width = scrolled + "%";
+      }
 
-function myFunction() {
-    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    var scrolled = (winScroll / height) * 100;
-    document.getElementById("myBar").style.width = scrolled + "%";
-}
+      function scrollFunction() {
+          if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+              document.getElementById("myBtn").style.display = "block";
+          } else {
+              document.getElementById("myBtn").style.display = "none";
+          }
+      }
 
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("myBtn").style.display = "block";
-    } else {
-        document.getElementById("myBtn").style.display = "none";
-    }
-}
+      // When the user clicks on the button, scroll to the top of the document
+      function topFunction() {
+          document.body.scrollTop = 0;
+          document.documentElement.scrollTop = 0;
+      }
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-}
+      $(document).ready(function() {
+          $('a[href^="#"]').on('click', function(e) {
+              e.preventDefault();
 
-$(document).ready(function() {
-    $('a[href^="#"]').on('click', function(e) {
-        e.preventDefault();
+              var target = this.hash,
+                  $target = $(target);
 
-        var target = this.hash,
-            $target = $(target);
-
-        $('html, body').stop().animate({
-            'scrollTop': $target.offset().top
-        }, 1200, 'swing', function() {
-            window.location.hash = target;
-        });
-    });
-});
+              $('html, body').stop().animate({
+                  'scrollTop': $target.offset().top
+              }, 2000, 'swing', function() {
+                  window.location.hash = target;
+              });
+          });
+      });
